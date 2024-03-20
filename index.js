5eca395f24b7f2f741cb91090249e1cbef6ade11
navin-ghimire <navinghimire143@gmail.com>
2024-03-20 19:10:52 +0545

    ch

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 
-let gee;
-const personName = 'world';
-const age = 90;
-const rating = 90.90;
-const isShow = true;
-const bee = false;
-const val = null;
-console.log(personName.toUpperCase());
-console.log(personName.concat(' NEPAL'));
-console.log(personName.repeat(5));
-console.log(val);
+// const firstName = 'rama';
 
-const person = {
-  name: 'ram',
-  age: 90,
-  eating: () => {
-    console.log('person eating')
-  }
+// const secondName = 'sama';
 
-};
+// const age = 90;
+
+// const whole = firstName + ' ' + secondName + ' ' + 'is' + ' ' + age + ' ' + 'years old';
+
+// console.log(whole);
+
+// const sentence = `${firstName.toUpperCase()} ${secondName.toUpperCase()} is ${age} years old`;
+// console.log(sentence);
+
+// ============================
+
+const temperature = 20;
+const value = 2;
+const pizza = 'pizza';
+const pasta = 'pasta';
+
+
+console.log(`temperature is ${temperature}C`);
+console.log(`${value} plus ${value} makes ${value + value}`);
+console.log(`I like ${pizza} and ${pasta}`);
+
+
+
+
+
+
+
