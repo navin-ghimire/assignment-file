f428cca8e6aaf186d0fecbfd0d690b9c68556862
navin-ghimire <navinghimire143@gmail.com>
2024-03-20 16:03:02 +0545

    ch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-my beautiful world navin ghimire
+
+let gee;
+const personName = 'world';
+const age = 90;
+const rating = 90.90;
+const isShow = true;
+const bee = false;
+const val = null;
+console.log(personName.toUpperCase());
+console.log(personName.concat(' NEPAL'));
+console.log(personName.repeat(5));
+console.log(val);
+
+const person = {
+  name: 'ram',
+  age: 90,
+  eating: () => {
+    console.log('person eating')
+  }
+
+};
