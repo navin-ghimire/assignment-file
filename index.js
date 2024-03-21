4f4817b395aff433691454bc2e87c11c606438c5
navin-ghimire <navinghimire143@gmail.com>
2024-03-22 00:54:10 +0545

    ch

--- index.js
+++ index.js
@@ -1,4 +1,149 @@
 
+//return function
+
+const nis = () => {
+  return 90 * 90;
+}
+
+const calc = nis();
+
+console.log(calc);
+
+
+
+
+// ================================
+
+// function addingfunc() {
+//   console.log('this func call');
+// }
+
+const adds = () => {
+  const a = 90;
+  console.log('this func call');
+  console.log(a + 190);
+}
+
+adds();
+
+
+// ======================
+//switch
+
+// const pos = 1;
+
+// switch (pos) {
+
+//   case 1:
+//     console.log('u got gold medal');
+//     break;
+//   case 2:
+//     console.log('u got bronze medal');
+//     break;
+//   case 3:
+//     console.log('u got silver medal');
+//     break;
+//   case 4:
+//   default:
+//     console.log('u got no medal');
+// }
+
+
+
+
+// ===========================
+//if else
+
+// const salary = 50000;
+// const expenses = 10000;
+// let total;
+
+// if (salary > expenses) {
+//   total = `you have saved ${salary - expenses} this month`;
+// }
+// else if (expenses > salary) {
+//   total = `you have lost ${expenses - salary} this month`;
+// }
+// else {
+//   total = `ur balance hasnt changed`
+// }
+
+// console.log(total);
+
+
+
+
+
+// ==========================
+
+// const age = 20;
+
+// if (age > 20) {
+//   result = 'you are child'
+// }
+// else if (age === 20) {
+//   result = 'you are in middle age'
+// }
+// else {
+//   result = 'ur age is grater then 20'
+// }
+
+// console.log(result);
+
+
+// ==========================
+
+
+// //falsy
+// const val = '';
+
+// //truly value
+// const val2 = [];
+// const val3 = {};
+
+// //falsy value
+// let nio;
+
+// //undefine null falsy value
+// const val2 = null;
+
+// //truly value
+// const a = 1;
+
+// //falsy value
+// const b = 0;
+
+
+
+
+
+
+
+
+// ==================
+//terninary operater
+
+
+
+
+// const salary = 60000;
+// const expenses = 20000;
+
+// let total;
+
+// if (salary > expenses) {
+//   total = `You have saved ${salary - expenses} this month`;
+// } else if (expenses > salary) {
+//   total = `You have lost ${expenses - salary} this month`;
+// } else {
+//   total = `Your balance hasn't changed`;
+// }
+
+// console.log(total);
+
+
+// =========================
+
 // const firstName = 'rama';
 
 // const secondName = 'sama';
@@ -14,18 +159,139 @@
 
 // ============================
 
-const temperature = 20;
-const value = 2;
-const pizza = 'pizza';
-const pasta = 'pasta';
-
-
-console.log(`temperature is ${temperature}C`);
-console.log(`${value} plus ${value} makes ${value + value}`);
-console.log(`I like ${pizza} and ${pasta}`);
 
 
 
+// const a = 90;
+// const b = 100;
+
+// const newWord = a > b ? 'milyo' : a === b ? 'also m' : 'milena';
+
+// console.log(newWord);
+
+
+
+
+
+// const a = 90;
+// const b = 100;
+
+// console.log(a > b || a === b || b > a);
+// console.log(a > b && a === b && b > a);
+
+
+
+// const a = 90;
+// const b = 100;
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a !== b);
+// console.log(a === b);
+
+
+
+// ==================================
+//assign operater
+
+
+
+// let x = 90;
+// //x -= 100;
+// x *= 100;
+// //x = x + 100;
+// //(x + = 100; same ho)
+
+// console.log(x)
+
+
+
+
+// let c = 90;
+// c = c + 1;
+// //(c++; same ho)
+
+// console.log(c);
+
+// const nios = 20 + 20 / (9 + 9);
+
+// console.log(nios);
+
+
+
+// const a = 90;
+// const b = 100;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(b % a);
+
+
+
+
+// ==============================
+//math object
+
+
+// const maths = Math.random() * 6 + 1;
+
+// console.log(Math.floor(maths));
+
+
+
+
+// const rating = 9976.4568;
+
+// console.log(Math.PI);
+// console.log(Math.floor(rating));
+// console.log(Math.ceil(rating));
+// console.log(Math.round(rating));
+
+// const rating = 9876.5868;
+
+// console.log(Math.round(rating));
+// console.log(Math.ceil(rating));
+// console.log(Math.floor(rating));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(4));
+// console.log(Math.max(66, 77, 99, 44));
+// console.log(Math.min(66, 77, 99, 44));
+
+
+//
+
+// =================================
+
+// const temperature = 20;
+// const value = 2;
+// const pizza = 'pizza';
+// const pasta = 'pasta';
+
+
+// console.log(`temperature is ${temperature}C`);
+// console.log(`${value} plus ${value} makes ${value + value}`);
+// console.log(`I like ${pizza} and ${pasta}`);
+
+
+// ==========================
+
+
+// const firstName = 'rama';
+
+// const secondName = 'sama';
+
+// const age = 90;
+
+// const whole = firstName + ' ' + secondName + ' ' + 'is' + ' ' + age + ' ' + 'years old';
+
+// console.log(whole);
+
+// const sentence = `${firstName.toUpperCase()} ${secondName.toUpperCase()} is ${age} years old`;
+// console.log(sentence);
 
 
 
